cab1befc16ebdf00dd0da7a571ef917acbf28d73
bright.ma <bright.ma@magesfc.com>
2023-04-21 22:36:45 +0800

    command: compile.go: add arg and flag

--- command/compile.go
+++ command/compile.go
@@ -1,7 +1,40 @@
 package command
 
-import "gopkg.in/alecthomas/kingpin.v2"
+import (
+	"fmt"
+	"github.com/golang108/drone-runner-exec/command/internal"
+	"gopkg.in/alecthomas/kingpin.v2"
+	"os"
+)
+
+type compileCommand struct {
+	*internal.Flags
+
+	Root    string
+	Source  *os.File
+	Environ map[string]string
+	Secrets map[string]string
+}
+
+func (c *compileCommand) run(*kingpin.ParseContext) error {
+	fmt.Println("compile the yaml file func run .....")
+	return nil
+}
 
 func registerCompile(app *kingpin.Application) {
+	c := new(compileCommand)
 
+	cmd := app.Command("compile", "compile the yaml file").
+		Action(c.run)
+
+	cmd.Arg("root", "root build directory").
+		Default("").
+		StringVar(&c.Root)
+
+	cmd.Arg("source", "source file location").
+		Default(".drone.yml").
+		FileVar(&c.Source)
+
+	// shared pipeline flags
+	c.Flags = internal.ParseFlags(cmd)
 }
