3711fbf4072704c7e858c1c054defab7f4b16717
bright.ma <bright.ma@magesfc.com>
2023-04-22 00:12:46 +0800

    command: update func run

--- command/exec.go
+++ command/exec.go
@@ -1,10 +1,28 @@
 package command
 
 import (
+	"context"
 	"fmt"
+	"io"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/golang108/drone-runner-exec/command/internal"
+	"github.com/golang108/drone-runner-exec/engine"
+	"github.com/golang108/drone-runner-exec/engine/compiler"
+	"github.com/golang108/drone-runner-exec/engine/resource"
+	"github.com/golang108/drone-runner-exec/runtime"
+
+	"github.com/drone/drone-go/drone"
+	"github.com/drone/envsubst"
+	"github.com/drone/runner-go/environ"
+	"github.com/drone/runner-go/manifest"
+	"github.com/drone/runner-go/pipeline"
+	"github.com/drone/runner-go/pipeline/console"
+	"github.com/drone/runner-go/secret"
+	"github.com/drone/signal"
+
 	"github.com/mattn/go-isatty"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
@@ -21,7 +39,106 @@ type execCommand struct {
 }
 
 func (c *execCommand) run(*kingpin.ParseContext) error {
-	fmt.Println("executes a pipeline func run.....")
+	rawsource, err := io.ReadAll(c.Source)
+	if err != nil {
+		return err
+	}
+
+	envs := environ.Combine(
+	// empty envs
+	)
+	// string substitution function ensures that string
+	// replacement variables are escaped and quoted if they
+	// contain newlines.
+	subf := func(k string) string {
+		v := envs[k]
+		if strings.Contains(v, "\n") {
+			v = fmt.Sprintf("%q", v)
+		}
+		return v
+	}
+	// evaluates string replacement expressions and returns an
+	// update configuration.
+	config, err := envsubst.Eval(string(rawsource), subf)
+	if err != nil {
+		return err
+	}
+
+	// parse and lint the configuration.
+	manifest, err := manifest.ParseString(config)
+	if err != nil {
+		return err
+	}
+
+	// a configuration can contain multiple pipelines.
+	// get a specific pipeline resource for execution.
+	resource, err := resource.Lookup(c.Stage.Name, manifest)
+	if err != nil {
+		return err
+	}
+
+	// compile the pipeline to an intermediate representation.
+	comp := &compiler.Compiler{
+		Pipeline: resource,
+		Manifest: manifest,
+		Build:    c.Build,
+		Netrc:    c.Netrc,
+		Repo:     c.Repo,
+		Stage:    c.Stage,
+		System:   c.System,
+		Environ:  c.Environ,
+		Secret:   secret.StaticVars(c.Secrets),
+		Root:     c.Root,
+	}
+	spec := comp.Compile(nocontext)
+
+	fmt.Println(spec)
+	// create a step object for each pipeline step.
+	for _, step := range spec.Steps {
+		if step.RunPolicy == engine.RunNever {
+			continue
+		}
+		c.Stage.Steps = append(c.Stage.Steps, &drone.Step{
+			StageID:   c.Stage.ID,
+			Number:    len(c.Stage.Steps) + 1,
+			Name:      step.Name,
+			Status:    drone.StatusPending,
+			ErrIgnore: step.IgnoreErr,
+		})
+	}
+
+	// configures the pipeline timeout.
+	timeout := time.Duration(c.Repo.Timeout) * time.Minute
+	ctx, cancel := context.WithTimeout(nocontext, timeout)
+	defer cancel()
+
+	// listen for operating system signals and cancel execution
+	// when received.
+	ctx = signal.WithContextFunc(ctx, func() {
+		println("received signal, terminating process")
+		cancel()
+	})
+
+	state := &pipeline.State{
+		Build:  c.Build,
+		Stage:  c.Stage,
+		Repo:   c.Repo,
+		System: c.System,
+	}
+	err = runtime.NewExecer(
+		pipeline.NopReporter(),
+		console.New(c.Pretty),
+		engine.New(),
+		c.Procs,
+	).Exec(ctx, spec, state)
+	if err != nil {
+		return err
+	}
+	switch state.Stage.Status {
+	case drone.StatusError, drone.StatusFailing:
+		os.Exit(1)
+	}
+
 	return nil
 }
 
