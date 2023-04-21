53ab4d1963410140b1811522db0d983a5a0d888b
bright.ma <bright.ma@magesfc.com>
2023-04-22 00:12:19 +0800

    engine: add engine files

---
+++ engine/compiler/vars.go
@@ -0,0 +1,18 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Polyform License
+// that can be found in the LICENSE file.
+
+//go:build !windows
+// +build !windows
+
+package compiler
+
+// netrc filename
+const netrc = ".netrc"
+
+// parameters that may be useful or required by child processes
+// to successfully execute.
+var hostVars = []string{
+	"PATH",
+	"USER",
+}
